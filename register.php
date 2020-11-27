2516d7854ed135b35df6bdafa2eaa6933e7f6011
BrunoSerro <55842349+BrunoSerro@users.noreply.github.com>
2020-11-27 16:04:02 +0000

    Update register.php

--- register.php
+++ register.php
@@ -3,12 +3,8 @@
 include 'db.php';
 require_once('/usr/share/php/smarty/libs/Smarty.class.php');
 
-  $smarty = new Smarty();
 
-  $smarty->template_dir = 'templates';
-  $smarty->compile_dir = 'templates_c';
-  $smarty->cache_dir = 'cache';
-  $smarty->config_dir = 'configs';
+$smarty = buildSmarty();
 
 //Array for menus
 $Menus = array (
@@ -28,26 +24,9 @@ $Form_V = array (
   "Email" => ""
 );
 
-  foreach ($Menus as $key => $value) {
-    $smarty->assign($key,$value);
-  }
-
-  $smarty->assign('UserName',$Form_V['Username']);
-  $smarty->assign('Email',$Form_V['Email']);
-  $smarty->assign('isError',$Form_V['isError']);
-  $smarty->assign('Error',$Form_V['ErrorTxt']);
-
-  $smarty->assign('pwd',"");
-  $smarty->assign('cfmpwd',"");
-  
-  $smarty->display('templates/register_template.tpl');
-
-
-
 $Form_V = checkREQUEST($Form_V);
-$smarty->assign($Menus,$Form_V,$smarty);
-$smarty->display($smarty);
-
+smartyAssign($Menus,$Form_V,$smarty);
+smartyDisplay($smarty);
 
 function checkREQUEST($Form_V) {
 
@@ -67,19 +46,20 @@ function errorCase($Form_V) {
         $Form_V['ErrorTxt'] = "Todos os campos devem ser preenchidos";
         break;
     case 1:
-        $Form_V['ErrorTxt'] = "Email já existe na base de dados";
+        $Form_V['ErrorTxt'] = "Email já se econtra registado";
         break;
     case 2:
         $Form_V['ErrorTxt'] = "Email tem formato incorrecto";
         break;
-    case 3:
+    case 4:
         //Este erro nao deve aperecer pois o HTML pede todos os campos
         $Form_V['ErrorTxt'] = "Password em branco";
         break;
-    case 4:
+    case 5:
         $Form_V['ErrorTxt'] = "Passwords não coincidem";
         break;
   }
+
   $Form_V['isError'] = 1;
   return $Form_V;
 }
@@ -92,4 +72,36 @@ function fillCorrect($Form_V){
   return $Form_V;
 }
 
+function buildSmarty(){
+
+  $smarty = new Smarty();
+
+  $smarty->template_dir = 'templates';
+  $smarty->compile_dir = 'templates_c';
+  $smarty->cache_dir = 'cache';
+  $smarty->config_dir = 'configs';
+  return $smarty;
+
+}
+
+function smartyAssign($Menus,$Form_V,$smarty) {
+
+  foreach ($Menus as $key => $value) {
+    $smarty->assign($key,$value);
+  }
+
+  $smarty->assign('UserName',$Form_V['Username']);
+  $smarty->assign('Email',$Form_V['Email']);
+  $smarty->assign('isError',$Form_V['isError']);
+  $smarty->assign('Error',$Form_V['ErrorTxt']);
+
+  $smarty->assign('pwd',"");
+  $smarty->assign('cfmpwd',"");
+  
+}
+
+function smartyDisplay($smarty) {
+  // Mostra a tabela
+  $smarty->display('templates/register_template.tpl');
+}
 ?>
